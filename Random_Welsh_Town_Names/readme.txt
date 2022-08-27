108b5056f33afed012e45800f4f6752d7d40d1ed
Andrew Miller <A.J.Miller@bcs.org.uk>
2022-08-28 00:16:44 +0100

    Changed the build process to build a tar file

---
+++ Random_Welsh_Town_Names/readme.txt
@@ -0,0 +1,5 @@
+Random Welsh Town Names by Andrew Miller © 2022
+
+Version 0.1.0
+
+Random Welsh Town Names is a newgrf for OpenTTD that generates Welsh town names from a list of prefixes and suffixes.
--- make.bat
+++ make.bat
@@ -1,5 +1,6 @@
 @echo off
 
-nmlc --grf Random_Welsh_Town_Names.grf Random_Welsh_Town_Names.nml
+nmlc --grf Random_Welsh_Town_Names\Random_Welsh_Town_Names.grf Random_Welsh_Town_Names.nml
+tar -cvf Random_Welsh_Town_Names.tar Random_Welsh_Town_Names
 
-move Random_Welsh_Town_Names.grf %OPENTTD_DIR%\newgrf
+move Random_Welsh_Town_Names.tar %OPENTTD_DIR%\newgrf
